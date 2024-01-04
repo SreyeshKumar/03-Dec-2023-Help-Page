4dcf83fec3c7647304594fd5388708c2c812fd07
SreyeshKumar <153359505+SreyeshKumar@users.noreply.github.com>
2024-01-04 12:23:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function formOption(){
+	document.getElementById("chatBtn").classList.remove("d-none");
+	document.getElementById("whatsAppIcon").classList.add("d-none");
+};
+function whatsAppOption(){
+	document.getElementById("chatBtn").classList.add("d-none");
+	document.getElementById("whatsAppIcon").classList.remove("d-none");
+}
